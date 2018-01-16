8031863741a8b665f0aef9cb2009ff502011d68c
Jason-Xi <Jason-Xi@users.noreply.github.com>
2018-01-16 13:54:55 +0800

    Initial version

--- util/util.py
+++ util/util.py
@@ -23,3 +23,25 @@ def cvtBKchar2WHT(img_src):
         # cv2.imshow("img_src1",img_src)
         # cv2.waitKey()
         return img_src
+
+def cvtBKchar2WHTs(img_srcs):
+    img_grays = []
+    img_outputs = []
+    black_background = 0
+    for img_src in img_srcs:
+        if len(img_src.shape) == 3:
+            img_src = cv2.cvtColor(img_src,cv2.COLOR_BGR2GRAY)
+
+        _, img_src = cv2.threshold(img_src, 255, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+
+        if img_src[0,img_src.shape[1]-1] == 0:  # if the left bottom is black
+            black_background = black_background + 1 # count the number of the black background
+        img_grays.append(img_src)
+    if len(img_grays)/2 - black_background > 0:  # if the background color of most Images are not black
+        for img_gray in img_grays:
+            img_gray = 255 - img_gray            # convert the color of the img
+            img_outputs.append(img_gray)
+
+        return img_outputs
+    else:
+        return img_grays
